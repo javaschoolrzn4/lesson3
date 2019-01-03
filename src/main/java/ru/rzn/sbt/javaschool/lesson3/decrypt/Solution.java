693840f11daf1b669993270d5042826502b55fa5
kaptsov-sa <Kaptsov.S.A@omega.sbrf.ru>
2019-01-03 12:53:21 +0300

    decrypt my

--- src/main/java/ru/rzn/sbt/javaschool/lesson3/decrypt/Solution.java
+++ src/main/java/ru/rzn/sbt/javaschool/lesson3/decrypt/Solution.java
@@ -15,6 +15,38 @@ public class Solution {
      * Не забудте проверку на null.
      */
     public static String decrypt(String encrypted){
-        return null;
+        if (encrypted == null) {
+            return null;
+        }
+        char[] arr = encrypted.toCharArray();
+        System.out.println(arr);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = convertSymbol(arr[i]);
+        }
+        System.out.println(arr);
+        return String.valueOf(arr);
+    }
+
+    private static char convertSymbol(char symbol) {
+        int item = (int) symbol;
+        boolean upper;
+        if (item >= 97 && item <= 122) {
+            upper = false;
+        } else if (item >= 65 && item <= 90) {
+            upper = true;
+        } else {
+            return symbol;
+        }
+        item -= 9;
+        if (upper) { // заглавная буква
+            if (item < 65) {
+                item = 90 - (65 - item) + 1;
+            }
+        } else { // строчная буква
+            if (item < 97) {
+                item = 122 - (97 - item) + 1;
+            }
+        }
+        return (char) item;
     }
 }
